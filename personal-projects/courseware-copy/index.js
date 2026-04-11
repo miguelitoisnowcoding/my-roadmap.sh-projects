9859e8520aff95aaab14879d08b3532185f904fa
Miguel Mansilla <70308781+miguelitoisprogramming@users.noreply.github.com>
2026-04-11 12:32:16 +0800

    Implemented Practice File for JavaScript

---
+++ personal-projects/courseware-copy/index.js
@@ -0,0 +1,4 @@
+console.log(`hello`);
+console.log(`I like ducks`);
+
+window.alert(`this is an alert`);
---
+++ personal-projects/javascript-practice/index.js
@@ -0,0 +1,9 @@
+let firstName = `Miguel`;
+let x = 1270;
+
+x += 1;
+let overEighteen = true;
+
+document.getElementById("p1").textContent = `Your name is ${firstName}`;
+document.getElementById("p2").textContent = `Your favourite number is ${x}`;
+document.getElementById("p3").textContent = `Is he over eighteen: ${overEighteen}`;
